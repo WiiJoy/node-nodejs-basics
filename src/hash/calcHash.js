f4e6efdf5fe0b1c6d56ce51de0dd1a2680482674
Vitalii Rogozin <wiification@gmail.com>
2025-04-24 21:13:45 +0500

    feat: add hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url'
+import { createHash } from 'crypto'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = fileURLToPath(new URL('.', import.meta.url))
+    const fileToRead = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+    const hash = createHash('sha256')
+
+    const readStream = fs.createReadStream(fileToRead)
+
+    readStream.on('data', (chunk) => {
+        hash.update(chunk)
+        console.log(hash.digest('hex'))
+    })
 };
 
 await calculateHash();
