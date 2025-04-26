9c22f6dee5e3c3b2481195c073a8f5e1a63d2029
Vitalii Rogozin <wiification@gmail.com>
2025-04-26 21:53:59 +0500

    feat: add solutions for cp subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { stdout, stdin } from 'process'
+import { spawn } from 'child_process'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = fileURLToPath(new URL('.', import.meta.url))
+    const scriptFile = path.join(__dirname, 'files', 'script.js')
+
+    const childProcess = spawn('node', [scriptFile, ...args], {
+        stdio: ['pipe', 'pipe', 'pipe']
+    })
+
+    stdin.pipe(childProcess.stdin)
+    childProcess.stdout.pipe(stdout)
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['hedgehog', 'duck', 'dog', 'cat'] );
