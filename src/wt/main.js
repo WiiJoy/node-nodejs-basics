b677fa865545a70e0aac474bf3253fac75c3affd
Vitalii Rogozin <wiification@gmail.com>
2025-04-26 16:07:17 +0500

    feat: add solution for workers subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { availableParallelism } from 'os'
+import { Worker } from 'worker_threads'
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = fileURLToPath(new URL('.', import.meta.url))
+    const worker = path.join(__dirname, 'worker.js')
+    const allWorkers = []
+    const result = []
+
+    const coreCount = availableParallelism()
+
+    for (let i = 0; i < coreCount; i++) {
+        const workerElem = new Worker(worker, {
+            workerData: 10 + i
+        })
+
+        workerElem.on('message', (message) => {
+            result[i] = {
+                status: 'resolved',
+                data: message
+            }
+        })
+
+        workerElem.on('error', (err) => {
+            result[i] = {
+                status: 'error',
+                data: null
+            }
+        })
+
+        allWorkers.push(
+            new Promise((res) => workerElem.on('exit', res))
+        )
+    }
+
+    await Promise.all(allWorkers)
+
+    console.log(result)
 };
 
 await performCalculations();
